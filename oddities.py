ec9af8a78c1f629b2c7b9b10790583a7e40895d5
Mi01sha01 <midegaurgesa@gmail.com>
2025-03-18 15:42:06 +0300

    Create oddities.py

---
+++ oddities.py
@@ -0,0 +1,12 @@
+n=int(input())
+for I in range (n):
+    num=int(input())
+    if num%2==0:
+        print(num,"is even!")
+    else:
+        print(num,"is odd!")    
+
+    
+        
+ 
+         
